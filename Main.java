a342181467707034afcad7976a4eba5d8dad20fa
KevinOnATree <kon100@student.hbuhsd.edu>
2025-09-29 21:46:19 +0000

    Fin

--- Main.java
+++ Main.java
@@ -5,8 +5,27 @@ public class Main
 {
   public static void main(String[] args)
   {
-    Scanner sc = new Scanner(System.in);
-   System.out.println("Output: \n" + lastFirstN("cream", "butter", 3));
+  Scanner sc = new Scanner(System.in);
+    System.out.println("Sample inputs: ");
+    String str1 = sc.nextLine();
+    String str2 = sc.nextLine();
+    int num = sc.nextInt();
+
+    sc.nextLine(); // scanner buffer
+
+    System.out.println("Output: \n" + lastFirstN(str1,str2,num));
+    System.out.println("Input: ");
+  
+    String str3 = sc.nextLine(); 
+    String str4 = sc.nextLine();
+
+   stringManip(str3, str4);
+   System.out.println("Removed Input: ");
+    String str5 = sc.nextLine(); 
+     System.out.println("Za Removed Input: ");
+    String str6 = sc.nextLine();
+  System.out.println("Removed Output: ");
+   System.out.println(removeStr(str5, str6));
   }
 
   /** Precondition: s1 and s2 are not null
@@ -27,15 +46,25 @@ public class Main
   */
   public static void stringManip(String s1, String s2)
   {
-  
-  }
+  String fin1 = new String(s1.toUpperCase());
+  String sub3 = s2.substring(0,1);
+  String sub4 = s2.substring(1,s2.length());
+  String fsub3 = sub3.toUpperCase();
+  String fin2 = new String(fsub3 + sub4);
+  System.out.println("toUpperCase Full Output: \n" + fin1);
+  System.out.println("First toUpperCase Output: \n" + fin2);
+  return;
+  }  
 
   /** Precondition: s1 and s2 are not null
   * Precondition: s2 is a substring of s1 and can be found at least one time in s1
   */
   public static String removeStr(String s1, String s2)
   {
-    String output = null;
-    return output;
-  }
+    int sindx = s1.indexOf(s2);
+    String s3 = s1.substring(0, sindx);
+    String s4 = s1.substring(s2.length() + 1, s1.length());
+    String fin5 = s3 + s4;
+  return fin5;
+}
 }
