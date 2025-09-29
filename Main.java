5fbdd9e00ff30b1500d07bf3d72f5bdced5d5337
KevinOnATree <kon100@student.hbuhsd.edu>
2025-09-29 18:21:48 +0000

    re

--- Main.java
+++ Main.java
@@ -1,16 +1,26 @@
+import java.util.Scanner;
+
+
 public class Main
 {
   public static void main(String[] args)
   {
-    // write your code here
+    Scanner sc = new Scanner(System.in);
+   System.out.println("Output: \n" + lastFirstN("cream", "butter", 3));
   }
 
   /** Precondition: s1 and s2 are not null
   * Precondition: 0 <= n <= min(s1.length(), s2.length()
   */
-  public static void lastFirstN(String s1, String s2, int n)
+  public static String lastFirstN(String s1, String s2, int n)
   {
-  
+    String sub1 = new String();
+    sub1 = s1.substring(s1.length() - n, s1.length());
+    String sub2 = new String();
+    sub2 = s2.substring(0, n);
+    String fin = new String(sub1 + sub2);
+
+    return fin;
   }
 
   /** Precondition: s1 and s2 are not null
